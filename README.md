4cef2db4791c39c1e678aff9ad804acbb8a0a46a
tornaco <tornaco@163.com>
2020-09-01 21:40:35 +0800

    Init project

--- README.md
+++ README.md
@@ -1,19 +1,3 @@
-# Riru - Template
-
-[Riru](https://github.com/RikkaApps/Riru) module template.
-
-## Build
-
-1. Rename `module.example.gradle` to `module.gradle`
-2. Replace module info in `module.gradle` (all lines end with `// FIXME replace with yours`)
-3. Write you codes
-4. Run gradle task `:module:assembleRelease` task from Android Studio or command line, zip will be saved in `out`.
-
-## Notes
-
-* DO NOT overwrite `android.os.SystemProperties#native_set` in core, or your data may be wiped
-  ([Detail info](https://github.com/RikkaApps/Riru/blob/v7/riru-core/jni/main/jni_native_method.cpp#L162-L176))
-  (If you really need to hook this, remember to clear exception)
-* DO NO print log (`__android_log_print`) in `nativeForkAndSpecialize(Pre/Post)` `nativeForkSystemServer(Pre/Post)` when in zygote process, or it may cause zygote not work
-  (magic not confirmed, [Detail info](https://github.com/RikkaApps/Riru/blob/77adfd6a4a6a81bfd20569c910bc4854f2f84f5e/riru-core/jni/main/jni_native_method.cpp#L55-L66))
-* Add `-ffixed-x18` to both compiler and linker parameter, or it will cause problems on Android Q (see template)# Thanox-Magisk
+# Thanox Magisk
+Patching Android frmamework with thanox core hooks.
+Module riru-core is needed.
---
+++ module/src/main/cpp/main.cpp
@@ -0,0 +1,82 @@
+#include <jni.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include "log.h"
+
+// You can remove functions you don't need
+
+extern "C" {
+#define EXPORT __attribute__((visibility("default"))) __attribute__((used))
+EXPORT void nativeForkAndSpecializePre(
+        JNIEnv *env, jclass clazz, jint *_uid, jint *gid, jintArray *gids, jint *runtimeFlags,
+        jobjectArray *rlimits, jint *mountExternal, jstring *seInfo, jstring *niceName,
+        jintArray *fdsToClose, jintArray *fdsToIgnore, jboolean *is_child_zygote,
+        jstring *instructionSet, jstring *appDataDir, jboolean *isTopApp,
+        jobjectArray *pkgDataInfoList,
+        jobjectArray *whitelistedDataInfoList, jboolean *bindMountAppDataDirs,
+        jboolean *bindMountAppStorageDirs) {
+}
+
+EXPORT int nativeForkAndSpecializePost(JNIEnv *env, jclass clazz, jint res) {
+    if (res == 0) {
+        // in app process
+    } else {
+        // in zygote process, res is child pid
+        // don't print log here, see https://github.com/RikkaApps/Riru/blob/77adfd6a4a6a81bfd20569c910bc4854f2f84f5e/riru-core/jni/main/jni_native_method.cpp#L55-L66
+    }
+    return 0;
+}
+
+EXPORT __attribute__((visibility("default"))) void specializeAppProcessPre(
+        JNIEnv *env, jclass clazz, jint *_uid, jint *gid, jintArray *gids, jint *runtimeFlags,
+        jobjectArray *rlimits, jint *mountExternal, jstring *seInfo, jstring *niceName,
+        jboolean *startChildZygote, jstring *instructionSet, jstring *appDataDir,
+        jboolean *isTopApp, jobjectArray *pkgDataInfoList, jobjectArray *whitelistedDataInfoList,
+        jboolean *bindMountAppDataDirs, jboolean *bindMountAppStorageDirs) {
+    // added from Android 10, but disabled at least in Google Pixel devices
+}
+
+EXPORT __attribute__((visibility("default"))) int specializeAppProcessPost(
+        JNIEnv *env, jclass clazz) {
+    // added from Android 10, but disabled at least in Google Pixel devices
+    return 0;
+}
+
+EXPORT void nativeForkSystemServerPre(
+        JNIEnv *env, jclass clazz, uid_t *uid, gid_t *gid, jintArray *gids, jint *runtimeFlags,
+        jobjectArray *rlimits, jlong *permittedCapabilities, jlong *effectiveCapabilities) {
+
+}
+
+EXPORT int nativeForkSystemServerPost(JNIEnv *env, jclass clazz, jint res) {
+    if (res == 0) {
+        // in system server process
+        char buffer[4096];
+        char *p = NULL;
+
+        strcpy(buffer, (p = getenv("SYSTEMSERVERCLASSPATH")) ? p : "");
+        // /system/framework/services.jar:/system/framework/ethernet-service.jar:/system/framework/wifi-service.jar:/system/framework/com.android.location.provider.jar
+        // Test.
+        setenv("SYSTEMSERVERCLASSPATH",
+               "/system/framework/services-thanox.jar:/system/framework/ethernet-service.jar:/system/framework/wifi-service.jar:/system/framework/com.android.location.provider.jar",
+               1);
+        LOGI("nativeForkSystemServerPost, SYSTEMSERVERCLASSPATH= %s", getenv("SYSTEMSERVERCLASSPATH"));
+        LOGI("nativeForkSystemServerPost, BOOTCLASSPATH= %s", getenv("BOOTCLASSPATH"));
+    } else {
+        // in zygote process, res is child pid
+        // don't print log here, see https://github.com/RikkaApps/Riru/blob/77adfd6a4a6a81bfd20569c910bc4854f2f84f5e/riru-core/jni/main/jni_native_method.cpp#L55-L66
+    }
+    return 0;
+}
+
+EXPORT int shouldSkipUid(int uid) {
+    // by default, Riru only call module functions in "normal app processes" (10000 <= uid % 100000 <= 19999)
+    // false = don't skip
+    return false;
+}
+
+EXPORT void onModuleLoaded() {
+    // called when the shared library of Riru core is loaded
+}
+}
